33879a2a34de0765e198df020df86162940f191e
nikitagagua <2gagua121@gmail.com>
2024-04-02 01:32:28 +0300

    fix: fixed problem with mapper bean

--- src/main/java/com/example/testtask/TestTaskApplication.java
+++ src/main/java/com/example/testtask/TestTaskApplication.java
@@ -1,9 +1,16 @@
 package com.example.testtask;
 
+import com.example.testtask.service.impl.BookstoreServiceImpl;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.testtask.mapper")
 public class TestTaskApplication {
 
 	public static void main(String[] args) {
